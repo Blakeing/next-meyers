a3ce0ff7d7cd6d99a8bcb974ef7b365b808e2614
Blake Ingenthron <blakeingenthron@gmail.com>
2021-05-31 23:05:00 -0500

    added cancelled/ success

---
+++ pages/cancelled.js
@@ -0,0 +1,10 @@
+const Cancelled = () => {
+  return (
+    <div className="relative w-full px-8 pt-8 pb-8 text-gray-600 bg-white rounded-md">
+      <h2 className="my-2 text-3xl font-md">Payment Cancelled!</h2>
+      <p>You have not been charged!</p>
+    </div>
+  );
+};
+
+export default Cancelled;
---
+++ pages/success.js
@@ -0,0 +1,10 @@
+const Success = () => {
+  return (
+    <div className="relative w-full px-8 pt-8 pb-8 text-gray-600 bg-white rounded-md">
+      <h2 className="my-2 text-3xl font-md">Payment successful</h2>
+      <p>Thank you for your purchase!</p>
+    </div>
+  );
+};
+
+export default Success;
